79babfca2f2dc01928d64e90c6d6bef360ff9664
JULIO CESAR STAMATI <135392044+JulioStamati@users.noreply.github.com>
2024-12-16 20:37:36 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+
+{
+    static void Main()
+    {
+        Console.WriteLine("Por favor, digite a sua idade:");
+
+        string input = Console.ReadLine();
+        Console.WriteLine(input);
+        
+        int idade;
+
+        if (int.TryParse(input, out idade))
+        {
+            if (idade < 18)
+            {
+                Console.WriteLine("Você é menor de idade");
+            }
+            else if (idade >= 18 && idade < 60)
+            {
+                Console.WriteLine("Você é adulto");
+            }
+
+            else
+            {
+                Console.WriteLine("Você é idoso");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Entrada inválida. Por favor, digite um número válido");
+        }
+      }
+    }
+
